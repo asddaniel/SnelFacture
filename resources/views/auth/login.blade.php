326da2ed1cc5273c0e5eebea90dea47a1eafe7db
Daniel Assani <devasddaniel@gmail.com>
2023-09-13 18:47:36 +0200

    add link register

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -32,11 +32,17 @@
             </label>
         </div>
 
+        <div class="pt-2 pb-2">
+            <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('register') }}">
+                {{ __('creer un compte ?') }}
+            </a>
+        </div>
         <div class="flex items-center justify-end mt-4">
             @if (Route::has('password.request'))
                 <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
                     {{ __('Forgot your password?') }}
                 </a>
+
             @endif
 
             <x-primary-button class="ml-3">
